f17e8160762bade8f56ee07707fa7c55491bfdcc
Lainitha Krishnakumar <krishnakumarlainitha68@gmail.com>
2025-07-28 21:46:10 +0530

    data types, variables

---
+++ Hello.java
@@ -0,0 +1,9 @@
+// system.out.print("Hello, World!"); ----jshell
+
+class Hello
+{
+    public static void main( String a[])
+    {
+        System.out.print("Hello,,,World!");
+    }
+}
---
+++ dataType.java
@@ -0,0 +1,21 @@
+class dataType {
+    public static void main(){
+
+        int a=24578;
+        long b=679890l;
+        short s=1234;
+        byte c=-127;
+        byte d=127;
+
+
+
+        float q = 3.14f;
+        double r=3.143;
+        char e='w';
+
+        boolean f=true;
+
+    }
+    }
+    
+
